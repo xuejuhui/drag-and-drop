6f8d0b3fb1d55ed98a44eefcabe3d6c67d51dc92
Ray Xue <xuejuhui@hotmail.com>
2019-04-15 17:55:01 -0400

    clean up a little

--- app.js
+++ app.js
@@ -1,64 +1,63 @@
-const empty = Array(54).fill("");
+const emptyArray = Array(54).fill("");
 
-const info = [1, 2, 3, 45, 6, 7, 8, 9, 0, 1, 2, 3, 4];
+const infos = [1, 2, 3, 45, 6, 7, 8, 9, 0, 1, 2, 3, 4];
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  let waiting = document.getElementsByClassName("waiting")[0];
-  let container = document.getElementsByClassName("container")[0];
-  for (let e of info) {
-    const div1 = document.createElement("div");
+  const waiting = document.getElementsByClassName("waiting")[0];
+  const container = document.getElementsByClassName("container")[0];
+  for (let e of infos) {
+    // create empty Node in DOM
+    const empty = document.createElement("div");
     const em = document.createTextNode("");
-    div1.appendChild(em);
-    // div1.draggable = true;
-    div1.className = "empty";
-    div1.ondragover = e => {
+    empty.appendChild(em);
+    empty.className = "empty";
+    empty.ondragover = e => {
       e.preventDefault();
     };
-    div1.ondrop = e => {
+    //Empty Node on drop append the elementNode to the target
+    empty.ondrop = e => {
       e.preventDefault();
       console.log(e);
       const src = e.dataTransfer.getData("Text");
       let srcNode = document.getElementById(src);
-      console.log(e.target);
-      // e.target = srcNode;
       e.target.appendChild(srcNode);
       e.stopPropagation();
     };
-    const div = document.createElement("div");
+    // Create Element from info array
+    const info = document.createElement("div");
     const newContent = document.createTextNode(e);
-    div.appendChild(newContent);
-    div.draggable = true;
-    div.className = "data";
-    div.id = Math.random() * (100000 - 0) + 0;
-    div.ondragstart = e => {
+    info.appendChild(newContent);
+    info.draggable = true;
+    info.className = "data";
+    info.id = Math.random() * (100000 - 0) + 0;
+
+    // On drap get the data and set it
+    info.ondragstart = e => {
       event.preventDefault();
-      console.log(e);
       e.dataTransfer.effectAllowed = "move";
       e.dataTransfer.setData("Text", e.target.id);
-      // e.dataTransfer.setDragImage(e.target, 0, 0);
     };
-    div1.appendChild(div);
-    waiting.appendChild(div1);
+    // append
+    empty.appendChild(info);
+    waiting.appendChild(empty);
   }
-  for (let e of empty) {
-    const div = document.createElement("div");
+  for (let e of emptyArray) {
+    // create emptyNode with emptyArray
+    const empty = document.createElement("div");
     const newContent = document.createTextNode(e);
-    div.appendChild(newContent);
-    // div.draggable = true;
-    div.className = "empty";
-    div.ondragover = e => {
+    empty.appendChild(newContent);
+    empty.className = "empty";
+    empty.ondragover = e => {
       e.preventDefault();
     };
-    div.ondrop = e => {
+    //Empty Node on drop append the elementNode to the target
+    empty.ondrop = e => {
       e.preventDefault();
-      console.log(e);
       const src = e.dataTransfer.getData("Text");
       let srcNode = document.getElementById(src);
-      console.log(e.target);
       e.target.appendChild(srcNode);
       e.stopPropagation();
     };
-    console.log(div);
-    container.appendChild(div);
+    container.appendChild(empty);
   }
 });
